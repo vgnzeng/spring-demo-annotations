eb7f280bde8d7370a99e17b33366c711b9611374
Guannian Zeng <vgnzeng@gmail.com>
2018-08-15 16:57:14 -0700

    read injected value from property file

--- src/com/luv2code/springdemo/SportConfig.java
+++ src/com/luv2code/springdemo/SportConfig.java
@@ -2,10 +2,26 @@ package com.luv2code.springdemo;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration
 // @ComponentScan("com.luv2code.springdemo")
+@PropertySource("classpath:sport.properties")
 public class SportConfig {
+	
+	// add support to resolve ${...} properties
+	//
+	// NOTE: THIS IS ONLY REQUIRED FOR OLD VERSIONS OF SPRING: v4.2 and lower
+	//
+	// This is NOT required if using Spring v4.3+
+	//
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer
+					propertySourcesPlaceHolderConfigurer() {
+		
+		return new PropertySourcesPlaceholderConfigurer();
+	}
 	
 	// define bean for our sad fortune service
 	@Bean
--- src/com/luv2code/springdemo/SwimCoach.java
+++ src/com/luv2code/springdemo/SwimCoach.java
@@ -1,9 +1,17 @@
 package com.luv2code.springdemo;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class SwimCoach implements Coach {
 
 	private FortuneService fortuneService;
 
+	@Value("${foo.email}")
+	private String email;
+	
+	@Value("${foo.team}")
+	private String team;
+	
 	public SwimCoach(FortuneService theFortuneService) {
 		fortuneService = theFortuneService;
 	}
@@ -17,6 +25,14 @@ public class SwimCoach implements Coach {
 	public String getDailyFortune() {
 		return fortuneService.getFortune();
 	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getTeam() {
+		return team;
+	}	
 
 }
 
