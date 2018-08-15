1ea508beb7ce68bfd8b980444b0f294721f55d83
Guannian Zeng <vgnzeng@gmail.com>
2018-08-15 13:15:29 -0700

    changed from contructor injection to setter injection

--- src/com/luv2code/springdemo/TennisCoach.java
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -3,16 +3,30 @@ package com.luv2code.springdemo;
 import org.springframework.stereotype.Component;
 import org.springframework.beans.factory.annotation.Autowired;
 
-@Component("mytennisCoach")
+@Component
 public class TennisCoach implements Coach {
 
 	private FortuneService fortuneService;
 	
+	// define a default constructor
+	public TennisCoach() {
+		System.out.println(">> TennisCoach: inside default constructor");
+	}
+
+	// define a setter method
+	@Autowired
+	public void setFortuneService(FortuneService theFortuneService) {
+		System.out.println(">> TennisCoach: inside setFortuneService() method");
+		this.fortuneService = theFortuneService;
+	}
+
+	/*
 	@Autowired
 	public TennisCoach(FortuneService theFortuneService) {
 		fortuneService = theFortuneService;
 	}
-
+	*/
+	
 	@Override
 	public String getDailyWorkout() {
 		return "Practice your backhand volley";
